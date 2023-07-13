6f58f9e230b363771bf060adc446bba657b498e7
Miro Benicio <mirodantas27@gmail.com>
2023-07-12 23:08:50 -0300

    feature: tabela de listagem com filter  e sort

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import { LoginComponent } from './auth/login/login.component';
 import { MatTableModule } from '@angular/material/table';
 import { RouterModule, Routes } from '@angular/router';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
@@ -19,6 +19,7 @@ import { DialogComponent } from './dialog/dialog.component';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatSortModule } from '@angular/material/sort';
 
 const appRoutes: Routes = [
   {
@@ -66,6 +67,7 @@ const appRoutes: Routes = [
     MatFormFieldModule,
     MatInputModule,
     ReactiveFormsModule,
+    MatSortModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/influencer/influencer-list/influencer-list.component.ts
+++ src/app/influencer/influencer-list/influencer-list.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Influencer } from '../../Influencer';
 import { InfluencerService } from '../influencer.service';
+import { MatSort } from '@angular/material/sort';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-influencer-list',
@@ -8,7 +11,21 @@ import { InfluencerService } from '../influencer.service';
   styleUrls: ['./influencer-list.component.css'],
 })
 export class InfluencerListComponent implements OnInit {
-  influencers: Influencer[] = [];
+  //influencers: Influencer[] = [];
+
+  displayedColumns: string[] = [
+    'id',
+    'nome',
+    'numero_inscritos',
+    'canal',
+    'plataforma',
+    'categoria',
+  ];
+
+  dataSource!: MatTableDataSource<Influencer>;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
 
   constructor(private influencerService: InfluencerService) {
     this.getInfluencers();
@@ -17,8 +34,20 @@ export class InfluencerListComponent implements OnInit {
   ngOnInit(): void {}
 
   getInfluencers(): void {
-    this.influencerService.getAll().subscribe((influencers) => {
-      this.influencers = influencers;
+    this.influencerService.getAll().subscribe({
+      next: (res) => {
+        this.dataSource = new MatTableDataSource(res);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+      },
     });
+  }
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
 }
